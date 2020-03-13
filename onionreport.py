9ec76a53d8ba6d1604571355f5dcb931b28f0afe
theschitz <theodore@theschitz.se>
2020-03-13 17:05:48 +0100

    add report class

---
+++ onionreport.py
@@ -0,0 +1,10 @@
+from onionlog import OnionLog, LogLine
+from onion_enums import LogLineType
+
+class OnionReport:
+    """
+    Report class for Onion Log
+    """
+    def __init__(self):
+
+        pass
