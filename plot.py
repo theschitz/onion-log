0ab1cae59cdbb4ac2a13d34e48b474b0c4f44c5d
theschitz <theodore@theschitz.se>
2020-05-17 00:11:34 +0200

    clean up

--- plot.py
+++ plot.py
@@ -13,17 +13,16 @@ class OnionPlot:
 def main():
     parser = OnionParser(datetime_str_fmt='%Y-%m-%d %H:%M:%S,%f')
     logs: List[OnionLog] = parser.parse('prod.log')
-    for log in (x for x in logs if x.code == 'CREATE-INV.CR-MEMO'): #and x.group == '{d2e1634a-b770-4536-9463-64221fb2bdb6}'):
-        print(log)        
-        plt.plot(range(len(log.executed_qty_list)), log.executed_qty_list, label=format_datetime(log.start_datetime))        
-        # plt.savefig(f'plot/{log.category}_{log.start_datetime}.png')
+    for log in (x for x in logs if x.code == 'CREATE-INV.CR-MEMO'):
+        print(log)
+        plt.plot(range(len(log.executed_qty_list)), log.executed_qty_list, label=format_datetime(log.start_datetime))
     if plt:
         plt.ylabel('Processed Contracts')
         plt.title('Create Invoices')
-        plt.xlabel('Log point (1 step = 15 minutes)')        
+        plt.xlabel('Log point (1 step = 15 minutes)')
         plt.legend()
         plt.grid(True)
-        plt.savefig(f'plot/{log.category}.png', dpi=1200)
+        plt.savefig(f'plot/{log.code}.png', dpi=300)
 
 def format_datetime(dt_str):
     dt = datetime.strptime(dt_str, '%Y-%m-%d %H:%M:%S,%f')
